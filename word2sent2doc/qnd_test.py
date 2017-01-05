1fdefbe9903d77172d7b3b8d94a024d05bb17a66
Yota Toyama <raviqqe@gmail.com>
2017-01-06 00:09:59 +0900

    s/constant/zeros/g

--- word2sent2doc/qnd_test.py
+++ word2sent2doc/qnd_test.py
@@ -9,7 +9,7 @@ def test_def_word2sent2doc():
     sys.argv = ["command", "--num_classes", "7", "--word_space_size", "10000"]
 
     model = qnd.def_word2sent2doc()
-    document = tf.constant([12, 34, 56])
+    document = tf.zeros([12, 34, 56])
 
     model(document, tf.zeros([12]))
     model(document, tf.zeros([12, 10]))
