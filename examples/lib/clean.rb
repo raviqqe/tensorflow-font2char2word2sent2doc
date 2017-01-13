bf73ce8730933a45330cecc29980817391b9254f
Yota Toyama <raviqqe@gmail.com>
2017-01-13 10:24:33 +0900

    Clean files per example directory

--- examples/lib/clean.rb
+++ examples/lib/clean.rb
@@ -7,7 +7,7 @@ end
 
 
 task :clobber do
-  sh "cd .. && git clean -dfx"
+  sh "git clean -dfx"
   shared_dataset_dir = "../#{DATASET_DIR}"
   sh "cd #{shared_dataset_dir} && rake clobber" if File.directory? shared_dataset_dir
 end
