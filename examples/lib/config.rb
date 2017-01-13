01f8a533457bdb736c6550d1a4daa6afcd7e0fe2
Yota Toyama <raviqqe@gmail.com>
2017-01-13 10:20:54 +0900

    Use dataset subproject

--- examples/lib/config.rb
+++ examples/lib/config.rb
@@ -1,3 +1,4 @@
 VAR_DIR = 'var'
 SHARED_VAR_DIR = "../#{VAR_DIR}"
+DATASET_DIR = "#{VAR_DIR}/dataset"
 OUTPUT_DIR = "#{VAR_DIR}/output"
--- examples/lib/dataset.rb
+++ examples/lib/dataset.rb
@@ -1,21 +1,11 @@
-require_relative './config.rb'
-
-IMDB_TAR = 'aclImdb_v1.tar.gz'
-DATASET_FILE = "#{SHARED_VAR_DIR}/#{IMDB_TAR}"
-DATASET_DIR = "#{SHARED_VAR_DIR}/aclImdb"
+require_relative './config'
 
 
-file DATASET_FILE do |t|
-  mkdir_p File.dirname(t.name)
-  sh "wget -O #{t.name} http://ai.stanford.edu/~amaas/data/sentiment/#{IMDB_TAR}"
+file VAR_DIR do
+  ln_s SHARED_VAR_DIR, VAR_DIR
 end
 
 
-directory DATASET_DIR => DATASET_FILE do |t|
-  sh "cd #{File.dirname(t.source)} && tar xf #{t.source}"
-end
-
-
-file VAR_DIR => DATASET_DIR do |t|
-  ln_s SHARED_VAR_DIR,t.name
+directory "#{DATASET_DIR}/aclImdb.json" => VAR_DIR do
+  sh "cd #{DATASET_DIR} && rake"
 end
