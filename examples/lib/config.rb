6eff33abaea29c7dc0045c0e1a1f18134555c0b6
Yota Toyama <raviqqe@gmail.com>
2017-01-17 09:16:11 +0900

    Make word.txt file task global

--- examples/lib/config.rb
+++ examples/lib/config.rb
@@ -1,6 +1,10 @@
 VAR_DIR = 'var'
 SHARED_VAR_DIR = "../#{VAR_DIR}"
 DATASET_DIR = "#{VAR_DIR}/dataset"
+
 TRAIN_FILE = "#{DATASET_DIR}/aclImdb.json/train/*.json"
 EVAL_FILE = "#{DATASET_DIR}/aclImdb.json/test/*.json"
+
+WORD_FILE = 'words.txt'
+
 OUTPUT_DIR = "#{VAR_DIR}/output"
--- examples/lib/dataset.rb
+++ examples/lib/dataset.rb
@@ -9,3 +9,10 @@ end
 task :dataset => VAR_DIR do
   sh "cd #{DATASET_DIR} && rake"
 end
+
+
+file WORD_FILE => :dataset do |t|
+  sh "echo '<null>' > #{t.name}"
+  sh "echo '<unknown>' >> #{t.name}"
+  sh "cat #{DATASET_DIR}/aclImdb/imdb.vocab >> #{t.name}"
+end
