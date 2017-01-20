1a7564bb031c1f32d5b31f7174d955740d71c221
Yota Toyama <raviqqe@gmail.com>
2017-01-20 13:05:39 +0900

    Visualize part of char embeddings

--- font2char2word2sent2doc/font2char.py
+++ font2char2word2sent2doc/font2char.py
@@ -5,4 +5,6 @@ import tensorflow as tf
 @ex.func_scope()
 def font2char(fonts, char_embedding_size):
     assert ex.static_rank(fonts) == 3
-    return ex.lenet(tf.expand_dims(fonts, -1), output_size=char_embedding_size)
+    h = ex.lenet(tf.expand_dims(fonts, -1), output_size=char_embedding_size)
+    ex.summary.image(tf.expand_dims(tf.expand_dims(h[:256], 0), 3))
+    return h
