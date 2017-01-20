a50c2c7dd2799164950b23d76f587b6b5aaae1b0
Yota Toyama <raviqqe@gmail.com>
2017-01-20 10:30:29 +0900

    Fix font2char test

--- font2char2word2sent2doc/font2char_test.py
+++ font2char2word2sent2doc/font2char_test.py
@@ -6,13 +6,4 @@ from . import font2char
 
 @ex.func_scope()
 def test_font2char():
-    font2char.font2char(tf.zeros([64, 224, 224]),
-                        char_embedding_size=100,
-                        mode=tf.contrib.learn.ModeKeys.TRAIN)
-
-
-@ex.func_scope()
-def test_vgg_16():
-    font2char.vgg_16(tf.zeros([64, 224, 224, 4]),
-                     output_size=100,
-                     mode=tf.contrib.learn.ModeKeys.TRAIN)
+    font2char.font2char(tf.zeros([64, 224, 224]), char_embedding_size=100)
