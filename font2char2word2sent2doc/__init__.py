e66898e155036d06db2d17f28e0ec9f0b45016e9
Yota Toyama <raviqqe@gmail.com>
2017-03-28 18:37:14 +0900

    Bump version

--- font2char2word2sent2doc/__init__.py
+++ font2char2word2sent2doc/__init__.py
@@ -5,4 +5,4 @@ from .font2char2word2sent2doc import (font2char2word2sent2doc,
 from .rd2sent2doc import rd2sent2doc
 from .ar2word2sent2doc import ar2word2sent2doc
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
