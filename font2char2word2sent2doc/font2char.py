5c70e06016a3e2527738562d163d3964dc629207
Yota Toyama <raviqqe@gmail.com>
2017-04-12 15:04:08 +0900

    Attend to images

--- font2char2word2sent2doc/font2char.py
+++ font2char2word2sent2doc/font2char.py
@@ -3,10 +3,11 @@ import tensorflow as tf
 
 
 @ex.func_scope()
-def font2char(fonts, num_layers=4, num_channels=32):
-    assert ex.static_rank(fonts) == 3
+def font2char(font, num_layers=4, num_channels=32):
+    assert ex.static_rank(font) == 3
 
-    h = tf.expand_dims(fonts, -1)
+    h = _attend_to_image(tf.expand_dims(font, -1), num_layers=num_layers - 1)
+
     for index, num_channels in enumerate([num_channels] * num_layers):
         h = tf.contrib.slim.conv2d(
             h, num_channels, 3, scope='conv{}'.format(index))
@@ -16,3 +17,34 @@ def font2char(fonts, num_layers=4, num_channels=32):
     ex.summary.image(tf.expand_dims(tf.expand_dims(h[:256], 0), 3))
 
     return h
+
+
+@ex.func_scope()
+def _attend_to_image(images, num_layers=3, num_channels=32):
+    assert ex.static_rank(images) == 4
+
+    return tf.transpose(
+        tf.transpose(images) *
+        tf.transpose(_calculate_attention(images,
+                                          num_layers=num_layers,
+                                          num_channels=num_channels)))
+
+
+@ex.func_scope()
+def _calculate_attention(images, num_layers=3, num_channels=32):
+    assert ex.static_rank(images) == 4
+
+    h = images
+
+    for index, num_channels in enumerate([num_channels] * num_layers):
+        h = tf.contrib.slim.conv2d(
+            h, num_channels, 3, scope='conv{}'.format(index))
+        h = tf.contrib.slim.max_pool2d(h, 2, 2, scope='pool{}'.format(index))
+
+    logits = tf.image.resize_nearest_neighbor(
+        tf.expand_dims(tf.reduce_sum(h, axis=3), axis=-1),
+        tf.shape(images)[1:3])
+
+    return tf.reshape(tf.nn.softmax(tf.reshape(logits,
+                                               [tf.shape(logits)[0], -1])),
+                      tf.shape(logits))
--- font2char2word2sent2doc/font2char_test.py
+++ font2char2word2sent2doc/font2char_test.py
@@ -6,4 +6,11 @@ from . import font2char
 
 @ex.func_scope()
 def test_font2char():
-    font2char.font2char(tf.zeros([64, 224, 224]))
+    assert ex.static_rank(font2char.font2char(tf.zeros([64, 224, 224]))) == 2
+
+
+@ex.func_scope()
+def test_attend_to_image():
+    assert (
+        ex.static_rank(font2char._attend_to_image(tf.zeros([64, 224, 224, 1])))
+        == 4)
